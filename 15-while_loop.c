dccedb46201450612ba44b09da29f012c0246a25
Ashlesh Basnet <ashleshbasnet@gmail.com>
2026-06-25 21:15:14 +0545

    While Loop

--- 15-while_loop.c
+++ 15-while_loop.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdbool.h>
+
 
 int main(){
 
@@ -9,9 +11,41 @@ int main(){
 
     while(number <= 0){
         printf("Enter a number greater than 0: ");
-        scanf("%d", &number); // Keeps continuing forever until we do a false statement!
+        scanf(" %d", &number); // Keeps continuing forever until we do a false statement!
         //                       Here it's a positive number
     }
+
+    do{
+        printf("Enter a number greater than 0: ");
+        scanf(" %d",&number);
+    }while(number<=0); //Post conditional while loop
+
+    number = -1;
+    while(number<=0){
+        printf("Enter a number less than 1: ");
+        scanf("%d", &number);
+        if(number==-1){
+            break;
+        }
+        else if(number ==0){
+            continue;
+        }
+    }
+
+    bool isRunning = true;
+    char response = '\0';
+
+    while(isRunning){
+        printf("You are playing a game\n");
+        printf("Would you like to continue? (Y = yes, N = no): ");
+        scanf(" %c", &response);
+
+        if (response !='Y' && response !='y'){
+            break; //You may also set isRunning to false, do note that break exists INSTANTLY
+            //       If you have stuff below this then DON'T USE BREAK!!!!!
+        }
+    }
+
 
     return 0;
 }
