58552874a253b03450a7f84395f3dc0a60a9953b
Ashlesh Basnet <ashleshbasnet@gmail.com>
2026-06-25 19:46:53 +0545

    Variable Scope

---
+++ 13-variable_scope.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+ 
+int add(int x, int y){
+    int result = x + y;
+    return result;
+}
+
+int main(){
+
+    // variable scope = Refers to where a variable is recognised and accessible
+    //                  Variables can share the same name if they're in different scopes {}
+    //                  Basically, local variables in each function can have the same name
+    //                  But they can't be the same in the same function! Redefinition isn't possible in C!!
+
+
+    int result = add(3,4); //Result here and result in add() are the same name! But have different scopes!!
+
+    printf("%d", result); //Therefore, a function can't see what's inside another function.
+    //                      They only pass in a parameter and get output, you could say they have the function abstracted!
+
+    return 0;
+}
