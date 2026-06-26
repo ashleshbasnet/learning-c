81eb60146601d45fa3fafa462f902c8f566bdec4
Ashlesh Basnet <ashleshbasnet@gmail.com>
2026-06-26 18:13:17 +0545

    Working on Break and Continue

---
+++ 17-break_and_continue.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int main(){
+
+    // break = break out of a loop (STOP)
+    // continue = Skip current cycle of a loop (SKIP)
+
+    for (int i=1; i<=10; i++){
+
+        
+    }
+
+    return 0;
+}
