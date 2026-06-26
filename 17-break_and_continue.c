15d6131d40176da6f343319d1796ecbcb759d5ab
Ashlesh Basnet <ashleshbasnet@gmail.com>
2026-06-26 18:35:55 +0545

    Break and Continue

--- 17-break_and_continue.c
+++ 17-break_and_continue.c
@@ -7,8 +7,20 @@ int main(){
     // continue = Skip current cycle of a loop (SKIP)
 
     for (int i=1; i<=10; i++){
+        if(i==4){
+            break; // Only counts until 3!
+        }
 
-        
+        printf("%d\n", i);
+    }
+    printf("\n");
+
+    for (int i=1; i<=10; i++){
+        if(i==4){
+            continue; // SKIPS the CYCLE 4 !!
+        }
+
+        printf("%d\n", i);
     }
 
     return 0;
